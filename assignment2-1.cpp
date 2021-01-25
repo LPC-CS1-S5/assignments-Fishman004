c6ab0b2150cc123e4854c4f1c1831c386bb97372
Fishman004 <dwfishman4@gmail.com>
2021-01-25 18:07:12 +0000

    assignment 2-1

---
+++ assignment2-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int checking = -20;
+  unsigned int miles = 4276;
+  long days = 18900;
+
+  cout << "We have made a long journey of " << miles << " miles. \n";
+  cout << "Our checking account balance is " << checking << ". \n";
+  cout << "About " << days << " days ago Columbus stood on this spot.";
+  return = 0;
+}
---
+++ assignment2-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  char c;
+
+  c = 74;
+  cout << c;
+
+  c=65;
+  cout << c;
+  
+  c=78;
+  cout << c << "\n";
+}
