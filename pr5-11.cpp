831aac123171f1a2d47a676f39a85663ba670d02
Fishman004 <dwfishman4@gmail.com>
2021-03-10 00:45:28 +0000

    pr5-11.cpp

---
+++ pr5-11.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+  int i = 0;
+  int n;
+  bool isprime = true;
+  while (i < 100) {
+    i++;
+    isprime = true;
+    //cout << "i = " << i << "\n";
+    for (n = 2; n <= i/2; n++){
+      if (i % n ==0){
+        isprime = false;
+        break;
+      }
+    }
+    if (isprime == true){
+      cout << i << "\n";
+    }
+    
+  }
+}
+
