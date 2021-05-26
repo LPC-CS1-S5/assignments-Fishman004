b80fb78f836d17af9c1ed28fc4ce513c5f2a41ec
Fishman004 <>
2021-05-26 07:20:44 +0000

    F4 and F5

--- F4!.cpp
+++ F4!.cpp
@@ -10,14 +10,12 @@ int main(){
   string cd = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quibus natura iure responderit non esse verum aliunde finem beate vivendi, a se principia rei gerendae peti; Quae enim adhuc protulisti, popularia sunt, ego autem a te elegantiora desidero. Duo Reges: constructio interrete. Tum Lucius: Mihi vero ista valde probata sunt, quod item fratri puto. Bestiarum vero nullum iudicium puto. Nihil enim iam habes, quod ad corpus referas; Deinde prima illa, quae in congressu solemus: Quid tu, inquit, huc? Et homini, qui ceteris animantibus plurimum praestat, praecipue a natura nihil datum esse dicemus?";
 
   cout << "Enter user string "; 
-  char cr[2];
-  cr[2] = userinput;
   char * pch;
   
-  pch = strchr(cd, cr);
+  pch = strchr(cd, userinput);
   while (pch!=NULL){
     printf ("Found %d at %i\n", cr, pch-cd+1);
-    pch = strchr(pch+1, cr);
+    pch = strchr(pch+1, userinput);
   }
   
     
